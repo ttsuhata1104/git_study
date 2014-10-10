393e181be25cae72a53da84fb396096c56c0b61a
Tomonori Tsuhata <tsuhata@soc.ait.kyushu-u.ac.jp>
2014-10-10 17:49:35 +0900

    change text.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>             
 int main()                     
 {                              
+    printf("Hi, everyone.\n");  
+    printf("My name is Tomonori Tsuhata.\n");  
     printf("hello, world\n");  
-    printf("hello, world2\n");  
-    printf("hello, world3\n");  
 		return 0;                  
 
 }                          
