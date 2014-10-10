e6161635c888b357cf2de6709f8c0c405bd98bf3
Yuki Abe <abe@soc.ait.kyushu-u.ac.jp>
2014-10-10 17:35:24 +0900

    Init. commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>             
+int main()                     
+{                              
+    printf("hello, world\n");  
+    printf("hello, world2\n");  
+    printf("hello, world3\n");  
+		return 0;                  
+
+}                          
+
