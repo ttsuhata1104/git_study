96aeafcb71029c5e83c119fb645a77d20f6289d7
Yuki Abe <abe@soc.ait.kyushu-u.ac.jp>
2014-10-10 17:48:58 +0900

    Add message.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main()
     printf("hello, world\n");  
     printf("hello, world2\n");  
     printf("hello, world3\n");  
+    printf("Yuki Abe\n");  
 		return 0;                  
 
 }                          
